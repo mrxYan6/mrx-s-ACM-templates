5df06231a60bba02e0e6f0c4de1a605b7c97055a
mrxYan6 <1827732507@qq.com>
2023-04-09 01:15:56 +0800

    optmize sieve

--- Math/Sieve.cpp
+++ Math/Sieve.cpp
@@ -1,16 +1,23 @@
-phi[1] = 1;
-for(int i = 2;i <= N;++i){
-	if(!vis[i]){
-		pri[++cnt] = i;
-		phi[i] = i - 1;
-	}
-	for(int j = 1;j <= cnt && i * pri[j] <= N;++j){
-		vis[i * pri[j]] = 1;
-		if(i % pri[j]){
-			phi[i * pri[j]] = phi[i] * (pri[j] - 1);
-		}else {
-			phi[i * pri[j]] = phi[i] * pri[j];
-			break;
-		}
-	}
+std::vector<int> minp, phi, primes;
+
+void sieve(int n) {
+    minp.resize(n + 1);
+    phi.resize(n + 1);
+    phi[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        if (!minp[i]) {
+            minp[i] = i;
+            phi[i] = i - 1;
+            primes.push_back(i);
+        }
+        for (auto p: primes) {
+            if (p * i > n)break;
+            minp[i * p] = 1;
+            if (p == minp[i]) {
+                phi[i * p] = phi[i] * p;
+                break;
+            }
+            phi[i * p] = phi[i] * (p - 1);
+        }
+    }
 }
