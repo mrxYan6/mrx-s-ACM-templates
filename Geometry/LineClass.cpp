b90438714388c3018ddd6a9c06913f7e989f28bf
mrxYan6 <1827732507@qq.com>
2023-07-08 22:57:27 +0800

    bug fix on double

--- Geometry/LineClass.cpp
+++ Geometry/LineClass.cpp
@@ -1,17 +1,9 @@
-#include <functional>
-#include <algorithm>
-#include <iostream>
-#include <numeric>
-#include <vector>
-#include <cmath>
-#include <queue>
-#include <array>
-#include <map>
 
 using i64 = long long;
-constexpr double eps = 1e-6;
+using db = double;
+constexpr db eps = 1e-6;
 
-int sgn(double x) {
+int sgn(db x) {
 	return x < -eps ? -1 : x > eps;
 }
 
@@ -42,12 +34,12 @@ struct Point {
 	Point& operator +=(const Point& rhs) { return (*this) = (*this) + rhs; }
 
 	template<class Y>
-	Point<double> operator *(const Y& rhs) const { return {x * rhs, y * rhs}; }
+	Point operator *(const Y& rhs) const { return {x * rhs, y * rhs}; }
 
 	template<class Y>
-	Point<double> operator /(const Y& rhs) { return {x / rhs, y / rhs}; }
+	Point<db> operator /(const Y& rhs) { return {x / rhs, y / rhs}; }
 
-	friend double abs(const Point& lhs) { return std::sqrt(lhs.x * lhs.x + lhs.y * lhs.y); }
+	friend db abs(const Point& lhs) { return std::sqrt(lhs.x * lhs.x + lhs.y * lhs.y); }
 
 	friend i64 abs2(const Point& lhs) { return (lhs.x * lhs.x + lhs.y * lhs.y); }
 
@@ -55,11 +47,11 @@ struct Point {
 
 	friend T dot(const Point& lhs, const Point& rhs) { return lhs.x * rhs.x + lhs.y * rhs.y; }
 
-	friend double angle(const Point& rhs) { return atan2(rhs.y, rhs.x); }
+	friend db angle(const Point& rhs) { return atan2(rhs.y, rhs.x); }
 
 	Point rotate90() const { return {y, x}; }
 
-	Point<double> rotate(double deg) { return {x * cos(deg) - y * sin(deg), x * sin(deg) + y * cos(deg)}; }
+	Point<db> rotate(db deg) { return {x * cos(deg) - y * sin(deg), x * sin(deg) + y * cos(deg)}; }
 
 	bool operator <(const Point& rhs) const { return sgn(x - rhs.x) == 0 ? sgn(y - rhs.y) < 0 : sgn(x - rhs.x) < 0; }
 
@@ -69,7 +61,7 @@ struct Point {
 };
 
 using Pl = Point<i64>;
-using Pd = Point<double>;
+using Pd = Point<db>;
 template<typename T>
 struct Line {
 	Point<T> a, v;
@@ -79,28 +71,28 @@ struct Line {
 	template<class Y>
 	Line(const Point<Y>& cp) : a(cp.a), v(cp.v) {}
 
-	Pd point(double t) {
+	Pd point(db t) {
 		return a + v * t;
 	}
 
-	friend Point<long double> intersection(const Line<T> lhs, const Line<T> rhs) {
-		long double t = (long double) cross(rhs.a - lhs.a, rhs.v) / cross(lhs.v, rhs.v);
-		return lhs.v * t + Point<double>(lhs.a);
+	friend Point<db> intersection(const Line<T> lhs, const Line<T> rhs) {
+		db t = (db) cross(rhs.a - lhs.a, rhs.v) / cross(lhs.v, rhs.v);
+		return lhs.v * t + Point<db>(lhs.a);
 	}
 
-	double dis(const Point<T>& rhs) {
+	db dis(const Point<T>& rhs) {
 		return std::abs(cross(rhs - a, v)) / v.abs();
 	}
 
-	Line rotate(double deg) {
-		Line<long double> ans(*this);
+	Line rotate(db deg) {
+		Line<db> ans(*this);
 		ans.v = Rotate(v, deg);
 		return ans;
 	}
 };
 
-using Pd = Point<double>;
-using Ld = Line<double>;
+using Pd = Point<db>;
+using Ld = Line<db>;
 
 bool isCross(Pd a, Pd b, Pd i, Pd j) {
 	return sgn(cross(i - a, j - i)) * sgn(cross(i - b, j - i)) == -1 && sgn(cross(b - i, a - b)) * sgn(cross(b - j, a - b)) == -1;
@@ -108,4 +100,8 @@ bool isCross(Pd a, Pd b, Pd i, Pd j) {
 
 bool onSeg(Pd a, Pd i, Pd j) {
 	return sgn(cross(i - a, j - a)) == 0 && sgn(dot(a - i, a - j)) < 0;
+}
+
+bool onLeft(Pd p, Ld l) {
+	return cross(l.v, p - l.a) > 0;
 }
--- Geometry/PointClass.cpp
+++ Geometry/PointClass.cpp
@@ -1,17 +1,9 @@
-#include <functional>
-#include <algorithm>
-#include <iostream>
-#include <numeric>
-#include <vector>
-#include <cmath>
-#include <queue>
-#include <array>
-#include <map>
 
 using i64 = long long;
-constexpr double eps = 1e-6;
+using db = double;
+constexpr db eps = 1e-6;
 
-int sgn(double x) {
+int sgn(db x) {
 	return x < -eps ? -1 : x > eps;
 }
 
@@ -42,12 +34,12 @@ struct Point {
 	Point& operator +=(const Point& rhs) { return (*this) = (*this) + rhs; }
 
 	template<class Y>
-	Point<double> operator *(const Y& rhs) const { return {x * rhs, y * rhs}; }
+	Point operator *(const Y& rhs) const { return {x * rhs, y * rhs}; }
 
 	template<class Y>
-	Point<double> operator /(const Y& rhs) { return {x / rhs, y / rhs}; }
+	Point<db> operator /(const Y& rhs) { return {x / rhs, y / rhs}; }
 
-	friend double abs(const Point& lhs) { return std::sqrt(lhs.x * lhs.x + lhs.y * lhs.y); }
+	friend db abs(const Point& lhs) { return std::sqrt(lhs.x * lhs.x + lhs.y * lhs.y); }
 
 	friend i64 abs2(const Point& lhs) { return (lhs.x * lhs.x + lhs.y * lhs.y); }
 
@@ -55,11 +47,11 @@ struct Point {
 
 	friend T dot(const Point& lhs, const Point& rhs) { return lhs.x * rhs.x + lhs.y * rhs.y; }
 
-	friend double angle(const Point& rhs) { return atan2(rhs.y, rhs.x); }
+	friend db angle(const Point& rhs) { return atan2(rhs.y, rhs.x); }
 
 	Point rotate90() const { return {y, x}; }
 
-	Point<double> rotate(double deg) { return {x * cos(deg) - y * sin(deg), x * sin(deg) + y * cos(deg)}; }
+	Point<db> rotate(db deg) { return {x * cos(deg) - y * sin(deg), x * sin(deg) + y * cos(deg)}; }
 
 	bool operator <(const Point& rhs) const { return sgn(x - rhs.x) == 0 ? sgn(y - rhs.y) < 0 : sgn(x - rhs.x) < 0; }
 
@@ -69,4 +61,4 @@ struct Point {
 };
 
 using Pl = Point<i64>;
-using Pd = Point<double>;
+using Pd = Point<db>;
