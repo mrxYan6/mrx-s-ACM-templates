e8f9ed2278ebdf952b9f4e7f5a1c5eace74bb91d
mrxYan6 <1827732507@qq.com>
2023-07-11 15:09:15 +0800

    add minkowski

---
+++ Geometry/minkowski.cpp
@@ -0,0 +1,15 @@
+std::vector<Pl> minkowski(std::vector<Pl> hullA, std::vector<Pl> hullB) {
+    int n = hullA.size(), m = hullB.size();
+    n--, m--;
+    std::vector<Pl> sega(n), segb(m);
+    for (int i = 1; i <= n; ++i) sega[i - 1] = hullA[i] - hullA[i - 1];
+    for (int i = 1; i <= m; ++i) segb[i - 1] = hullB[i] - hullB[i - 1];
+    std::vector<Pl> ans(n + m);
+    ans[0] = hullA[0] + hullB[0];
+    for (int i = 1, j = 0, k = 0; i < n + m; ++i) {
+        ans[i] = ans[i - 1];
+        if (j < n  && (k == m || sgn(cross(sega[j], segb[k])) >= 0)) ans[i] += sega[j++];
+        else ans[i] += segb[k++];
+    }
+    return ConvexHull(ans);
+}
