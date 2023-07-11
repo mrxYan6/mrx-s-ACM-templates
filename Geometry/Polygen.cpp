20f40b01696400d8efbf2f08b943823a530239d8
mrxYan6 <1827732507@qq.com>
2023-07-11 15:21:11 +0800

    format

--- Geometry/Polygen.cpp
+++ Geometry/Polygen.cpp
@@ -19,14 +19,14 @@ Pd polycenter(polygen a) {
 }
 
 //判点在是否在多边形内或边上
-bool point_in_polygon(Pd p, polygen a) {
+bool point_in_polygon(Pl p, std::vector<Pl> a) {
 	int n = a.size();
 	int s = 0;
 	for (int i = 0; i < n; i++) {
-		Pd u = a[i], v = a[(i + 1) % n];
+		auto u = a[i], v = a[(i + 1) % n];
 		if (onSeg(p, u, v))return true;
-		if (cmp(u.y, v.y) <= 0)std::swap(u, v);
-		if (cmp(p.y, u.y) > 0 || cmp(p.y, v.y) <= 0)continue;
+		if (sgn(u.y - v.y) <= 0)std::swap(u, v);
+		if (sgn(p.y - u.y) > 0 || sgn(p.y - v.y) <= 0)continue;
 		if (sgn(cross(v - p, u - p)) == 1)++s;
 	}
 	return s & 1;
--- Geometry/minkowski.cpp
+++ Geometry/minkowski.cpp
@@ -1,10 +1,11 @@
-std::vector<Pl> minkowski(std::vector<Pl> hullA, std::vector<Pl> hullB) {
+template <typename Point>
+std::vector<Point> minkowski(std::vector<Point> hullA, std::vector<Point> hullB) {
     int n = hullA.size(), m = hullB.size();
     n--, m--;
-    std::vector<Pl> sega(n), segb(m);
+    std::vector<Point> sega(n), segb(m);
     for (int i = 1; i <= n; ++i) sega[i - 1] = hullA[i] - hullA[i - 1];
     for (int i = 1; i <= m; ++i) segb[i - 1] = hullB[i] - hullB[i - 1];
-    std::vector<Pl> ans(n + m);
+    std::vector<Point> ans(n + m);
     ans[0] = hullA[0] + hullB[0];
     for (int i = 1, j = 0, k = 0; i < n + m; ++i) {
         ans[i] = ans[i - 1];
